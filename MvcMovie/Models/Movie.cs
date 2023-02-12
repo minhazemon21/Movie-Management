7e54b0a3fc303d12d4c04abb02a0dfcb745900ae
minhazemon21 <minhazkhondokar21@gmail.com>
2023-02-12 16:08:34 +0600

    first

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+
+    public class DbSet<T>
+    {
+    }
+}
