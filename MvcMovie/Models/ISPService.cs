575d11c343779c9d63a35b9db85c0bb893539c4c
minhazemon21 <minhazkhondokar21@gmail.com>
2023-03-02 12:55:07 +0600

    j

---
+++ MvcMovie/Models/ISPService.cs
@@ -0,0 +1,80 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace MvcMovie.Models
+//{
+//    public interface ISPService
+//    {
+
+//        DataSet GetDataWithParameter<TParamOType>(TParamOType target, string storeProcedureName) where TParamOType : class;
+//        DataSet GetDataWithoutParameter(string storeProcedureName);
+//        DataSet GetDataBySqlCommand(string sqlString);
+//        int GetSecurityModuleByControllerAction(string controller, string action);
+//        DateTime GetBusinessDay();
+//        int ExecuteStoredProcedure<TParamOType>(TParamOType target, string storedProcedure) where TParamOType : class;
+//        DataSet GetBrokerEmployeeForRegister();
+//    }
+//    public class SPService : ISPService
+//    {
+
+//        public DataSet GetDataWithParameter<TParamOType>(TParamOType target, string storeProcedureName) where TParamOType : class
+//        {
+//            using (var gbData = new UERPDataAccess())
+//            {
+//                return gbData.GetDataOnDateset(storeProcedureName, target);
+//            }
+//        }
+
+//        public DataSet GetDataWithoutParameter(string storeProcedureName)
+//        {
+//            using (var gbData = new UERPDataAccess())
+//            {
+//                return gbData.GetDataOnDatesetWithoutParam(storeProcedureName);
+//            }
+//        }
+
+//        public DataSet GetDataBySqlCommand(string sql)
+//        {
+//            using (var gbData = new UERPDataAccess())
+//            {
+//                return gbData.GetDataOnDatesetBySqlCommand(sql);
+//            }
+//        }
+//        public DateTime GetBusinessDay()
+//        {
+//            using (var gbData = new UERPDataAccess())
+//            {
+//                return DateTime.Parse(gbData.GetDataOnDatesetBySqlCommand("SELECT dbo.F_GET_BUSINESS_DAY()").Tables[0].Rows[0][0].ToString());
+//            }
+//        }
+
+//        public int ExecuteStoredProcedure<TParamOType>(TParamOType target, string storedProcedure) where TParamOType : class
+//        {
+//            using (var gbData = new UERPDataAccess())
+//            {
+//                return gbData.ExecuteNonQuery(storedProcedure, target);
+//            }
+//        }
+
+//        public int GetSecurityModuleByControllerAction(string controller, string action)
+//        {
+//            using (var gbData = new UERPDataAccess())
+//            {
+//                return int.Parse(gbData.GetDataOnDatesetBySqlCommand("SELECT AspNetSecurityModuleId FROM AspNetSecurityModule WHERE ControllerName='" + controller + "' AND ActionName='" + action + "'").Tables[0].Rows[0][0].ToString());
+//            }
+//        }
+
+//        public DataSet GetBrokerEmployeeForRegister()
+//        {
+//            var storeProcedureName = "USP_GetBrokerEmployeeForRegister";
+//            using (var gbData = new UERPDataAccess())
+//            {
+//                return gbData.GetDataOnDatesetWithoutParam(storeProcedureName);
+//            }
+//        }
+//    }
+//}
---
+++ MvcMovie/Models/UERPDataAccess.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Configuration;
+//using System.Linq;
+//using System.Web;
+
+//namespace MvcMovie.Models
+//{
+//    public class UERPDataAccess : DataAccessBase
+//    {
+//        public UERPDataAccess()
+//        {
+
+//        }
+
+//        protected override ConnectionStringSettings LoadConnectionStringSetting()
+//        {
+//            var connectionSrings = ConfigurationManager.ConnectionStrings["MvcMovieContext"];
+//            //TO DO: Load connection string values from web.config file, any other source.
+//            ConnectionStringSettings _dbConnectionStringSetting = connectionSrings;// new ConnectionStringSettings("DCURDB", @"Data Source=10.10.10.40;Initial Catalog=DCURDB;User ID=sa;Password=sa1234;Connect Timeout=45;Pooling=false", "System.Data.SqlClient");
+
+//            return _dbConnectionStringSetting;
+//        }
+
+//    }
+//}
+//}
